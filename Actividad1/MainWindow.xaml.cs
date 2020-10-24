0bd8606d7de3c03e83992b95214ad650dde619ef
adrianCG <adrianCG@mesi>
2020-10-24 11:05:17 +0200

    Agregar archivos de proyecto.

---
+++ Actividad1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+namespace Actividad1
+{
+    public partial class MainWindow : Window
+    {
+        public int NumeroAleatorioGenerado { get; private set; }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            NumeroAleatorioGenerado = GenerarNumeroAleatorio();
+        }
+
+        private int GenerarNumeroAleatorio()
+        {
+            Random semilla = new Random();
+            return semilla.Next(0, 101);
+        }
+
+        public void Comprobar_Click(object sender, RoutedEventArgs e)
+        {
+            int numeroElegido = int.Parse(eleccionUsuario.Text);
+            if (numeroElegido == NumeroAleatorioGenerado)
+                resultado.Text = "¡Has acertado!";
+            else if (numeroElegido < NumeroAleatorioGenerado)
+                resultado.Text = "Te has quedado corto";
+            else
+                resultado.Text = "Te has pasado";
+        }
+
+        public void Reiniciar_Click(object sender, RoutedEventArgs e)
+        {
+            NumeroAleatorioGenerado = GenerarNumeroAleatorio();
+            eleccionUsuario.Text = "";
+            resultado.Text = "";
+        }
+    }
+}
